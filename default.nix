bb42be8ae1583812c313db915d3b9ea67ba299c9
2gn <101851090+2gn@users.noreply.github.com>
2022-06-14 19:48:46 +0900

    let NUR import call receive a pkgs argument
    fixing the following error:
    error: NUR import call didn't receive a pkgs argument, but the evaluation of NUR's twogn repository requires it.                        
                                                                                                                                            
           This is either because                                                                                                           
             - You're trying to use a package from that repository, but didn't pass a `pkgs` argument to the NUR import.                    
               In that case, refer to the installation instructions at https://github.com/nix-community/nur#installation on how to properly 
    import NUR                                                                                                                              
                                                                                                                                            
             - You're trying to use a module/overlay from that repository, but it didn't properly declare their module.                     
               In that case, inform the maintainer of the repository: https://github.com/2gn/nur-package                                    
    (use '--show-trace' to show detailed location information

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  vscode-extensions = pkgs.callPackage ./pkgs/vscode-extensions { };
+  vscode-extensions = pkgs.callPackage ./pkgs/vscode-extensions { inherit pkgs; };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
